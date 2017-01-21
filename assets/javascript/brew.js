753121a37dfc488f444348a16a42e63d9840284a
Julie <jccheng@live.unc.edu>
2017-01-21 13:01:20 -0500

    updated brew.js, added zipcode.js + maps.js to index.html

--- assets/javascript/brew.js
+++ assets/javascript/brew.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
 
-  var color = "",
-      hoppiness = "",
-      abv = 0,
-      style = "",
-      zipcode = 27613,
-      brewery = [], // holds all brewery info
-      beer = [];
+// Variables
+brewery = [], // holds all brewery info
+beer = [];
 
 // THIS CODE IS AT LINE 30
   // //whatever id is for user inputs
@@ -23,26 +19,7 @@ $(document).ready(function() {
 
   // });//end of on click for user input
 
-  //may not be document click...change class or id to whatever submit button is?
-  $(document).on("click", "#submit", function() { // changed id to #submit in the middle
-    
-    //adjust ids to what individual user inputs will be
-    color = $("#color-input").val().trim();
-    hoppiness = $("#hoppiness-input").val().trim();
-    abv = $("#abv-input").val().trim();
-    style = $("#style-input").val().trim();
-    zipcode = $("#zipcode-input").val().trim();
-
-    //clear input fields
-    $("#color-input").val("");
-    $("#hoppiness-input").val("");
-    $("#abv-input").val("");
-    $("#style-input").val("");
-    $("#zipcode-input").val("");
-
-    //clear results div or however is displayed in html
-    $("#areaForResults").empty();  
-        
+  
     //URL based on zipcode input
     var queryURL = "http://utcors1.herokuapp.com/http://api.brewerydb.com/v2/locations/?key=9bb3bc076d572ad09b636ac87cc944c9&postalCode=" + zipcode;
        
@@ -128,7 +105,6 @@ $(document).ready(function() {
 
     return false;
 
-  });//end of on click for results probably not right because this was used on giphy.
 
 
 
---
+++ assets/javascript/zipcode.js
@@ -0,0 +1,48 @@
+// Variables
+	// User Input
+	var color = "",
+      	hoppiness = "",
+      	abv = 0,
+      	// style = "",
+      	zipcode = 0
+
+
+$(document).on("click", "#submitButton", function() { // changed id to #submit in the middle
+	//adjust ids to what individual user inputs will be
+    color = $(".colorButton").val().trim();
+    hoppiness = $(".hoppinessButton").val().trim();
+    abv = $(".ABVButton").val().trim();
+    // style = $("#style-input").val().trim();
+    zipcode = $("#zipCode-input").val().trim();
+
+    //clear input fields
+    $(".colorButton").val("");
+    $(".hoppinessButton").val("");
+    $(".ABVButton").val("");
+    // $("#style-input").val("");
+    $("#zipCode-input").val("");
+
+    //clear results div or however is displayed in html
+    $("#beerResults").empty(); 
+
+});//end of on click for results probably not right because this was used on giphy.
+
+
+
+var zipcode = 27613;
+var radius = 10
+
+var zipcodeURL = "http://utcors1.herokuapp.com/https://www.zipcodeapi.com/rest/b345qo4ak2sZAlNimGscHnkFGVUMDlRPpWVQlZ0F1sFBOsUa9mnQKHwqDrcgM45c/radius.json/" + zipcode + "/5/mile?minimal"
+
+$.ajax({
+	url: zipcodeURL,
+	method: "GET",
+	headers: { "X-Requested-With": "" } 
+}).done(function(response){
+
+		var zipCodes = response.zip_codes;
+		console.log(zipCodes)
+		// Stuff in here
+	})
+
+
