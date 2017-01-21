720dab6a162fd47dae1b7dcd9b5dd125d781513d
Meredith Erb <mereditherb@guest-2-00091.dhcp.unc.edu>
2017-01-21 11:28:24 -0500

    updated brew.js to match master

--- assets/javascript/brew.js
+++ assets/javascript/brew.js
@@ -1,110 +1,135 @@
 $(document).ready(function() {
 
-	var darkness = "";
-	var hoppiness = "";
-	var abv = 0;
-	var style = "";
-	var zipcode = 27613;
-	var breweryID = [];
+  var color = "",
+      hoppiness = "",
+      abv = 0,
+      style = "",
+      zipcode = 27613,
+      brewery = [], // holds all brewery info
+      beer = [];
 
-
-	//whatever id is for user inputs
-	$("#userinput").on("click", function() {
-	  //adjust ids to what individual user inputs will be
-	  darkness = $("#darkness-input").val().trim();
-	  hoppiness = $("#hoppiness-input").val().trim();
-      abv = $("#abv-input").val().trim();
-	  style = $("#style-input").val().trim();
-	  zipcode = $("#zipcode-input").val().trim();
-
-      //clear input fields
-      $("#darkness-input").val("");
-      $("#hoppiness-input").val("");
-	  $("#abv-input").val("");
-	  $("#style-input").val("");
-	  $("#zipcode-input").val("");
-      
-      return false;
+// THIS CODE IS AT LINE 30
+  // //whatever id is for user inputs
+  // $("#userinput").on("click", function() {
     
-    });//end of on click for user input
+  //   //adjust ids to what individual user inputs will be
+  //   color = $("#color-input").val().trim();
+  //   hoppiness = $("#hoppiness-input").val().trim();
+  //   abv = $("#abv-input").val().trim();
+  //   style = $("#style-input").val().trim();
+  //   zipcode = $("#zipcode-input").val().trim();
 
-	//may not be document click...change class or id to whatever submit button is?
-  $(document).on("click", "#example", function() {
-  	//clear results div or however is displayed in html
-    // $("#areaForResults").empty();  
-    	//URL based on zipcode input
-    var queryURL = "http://api.brewerydb.com/v2/locations/?key=9bb3bc076d572ad09b636ac87cc944c9&postalCode=" + zipcode;
-    	//URL for brewery based on queryURL to get beer at brewery to then search for other input
-    var queryBreweryURL = "http://api.brewerydb.com/v2/brewery/" + breweryID + "/beers/?key=9bb3bc076d572ad09b636ac87cc944c9"
-    	//MAY NOT NEED NOW! url for finding the beer information
-	// var queryBeerURL = "http://api.brewerydb.com/v2/beers/?key=9bb3bc076d572ad09b636ac87cc944c9&abv=";
-		//must we define all items we are searching for in url?
+  //   return false;
 
-		
+  // });//end of on click for user input
 
-	$.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response){
+  //may not be document click...change class or id to whatever submit button is?
+  $(document).on("click", "#submit", function() { // changed id to #submit in the middle
+    
+    //adjust ids to what individual user inputs will be
+    color = $("#color-input").val().trim();
+    hoppiness = $("#hoppiness-input").val().trim();
+    abv = $("#abv-input").val().trim();
+    style = $("#style-input").val().trim();
+    zipcode = $("#zipcode-input").val().trim();
 
-        	var breweryResult = response.data;
+    //clear input fields
+    $("#color-input").val("");
+    $("#hoppiness-input").val("");
+    $("#abv-input").val("");
+    $("#style-input").val("");
+    $("#zipcode-input").val("");
 
-        	for (var i = 0; i < breweryResult.length; i++){
+    //clear results div or however is displayed in html
+    $("#areaForResults").empty();  
+        
+    //URL based on zipcode input
+    var queryURL = "http://utcors1.herokuapp.com/http://api.brewerydb.com/v2/locations/?key=9bb3bc076d572ad09b636ac87cc944c9&postalCode=" + zipcode;
+       
+    // var queryBeerURL = "http://api.brewerydb.com/v2/beers/?key=9bb3bc076d572ad09b636ac87cc944c9&abv=";
+    //must we define all items we are searching for in url?
 
-        		breweryInfo = {"id": breweryResult[i].id,
-        						"name": breweryResult[i].name,
-        						"streetName": breweryResult[i].streetAddress,
-        						"state": breweryResult[i].region,
-        						"locality": breweryResult[i].locality 
-        					  }
-        		breweryID.push(breweryInfo);
-        		console.log(breweryInfo);
-        			alert("hello");
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      headers: { "X-Requested-With": "" }
+    }).done(function(response){
 
-        		console.log(breweryID);
+      var breweryResult = response.data; // array with brewery info as objects
 
-        	}//end of for loop 
+      console.log(breweryResult)
 
-        	// beerFunction();
-        })//end of function(response)
+      for (var i = 0; i < breweryResult.length; i++){ // for-loop to add all breweries into an array
 
-    // function beerFunction() {
+        breweryInfo = {"id" : breweryResult[i].brewery.id,
+                      "name" : breweryResult[i].brewery.name, 
+                      "streetName" : breweryResult[i].streetAddress,
+                      "state" : breweryResult[i].region,
+                      "locality": breweryResult[i].locality 
+                      } //end of breweryInfo object
 
-    // 	$.ajax({
-    //       url: queryBreweryURL,
-    //       method: "GET"
-    //     }).done(function(response){
+        brewery.push(breweryInfo); // pushes current brewery's entire info (stored as object) into array
 
-    //     	var beerResult = response.data;
+        // won't be in final code i think... i made this to see what is in the brewery array
+        // $("#areaForResults").append("Brewery #" + i + "- id: " + breweryInfo.id + " name: " + breweryInfo.name + "<br>")
 
-    //     	for (var i = 0; i < beerResult.length; i++){
-        		
+      } //end of for loop 
 
-    //     	}//end of for loop
+      beerFunction();
+    })//end of function(response)
 
-    //     	//add in other URL using breweryID to do for loop for other info.
-    //     		var beerResult = response.data;
+      function beerFunction() { 
+        
 
-    //     		for (var j = 0; j < beerResult.length; j++) {
+        for (var i = 0; i < brewery.length; i++){ // for loop to make sure we go through all breweries
+          var beerDiv = $("<div class='beerOptions'>").append("Brewery #" + i + " - ID: " + brewery[i].id + "| Name: " + brewery[i].name + "<br>")
+          $("#beerResults").append(beerDiv);
 
-    //     			//if statements here for user input meeting criteria?
-        			
-    //     			var beerDiv = "<div class = 'beerOptions'>";
-
-    //     			abv = beerResult[i].abv;
-    //     			hoppiness = beerResult[i].style.ibuMin;//need one for max
-    //     			darkness = beerResult[i].style.srmMin; //need one for max
-    //     			style = beerResult[i].style.name //only if decide to do style and if figure out way to do user input contains name
-    //     			var p = $("<p>").text("Beers: " + abv + hoppiness + darkness +style);
-
-    //     			beerDiv.append(p);
-    //     			$("#id of beer area in html").append(beerDiv);
-    //     		}
-    // 	}//end of beerFunction
+          //URL for brewery based on queryURL to get beer at brewery to then search for other input
+          var queryBreweryURL = "http://utcors1.herokuapp.com/http://api.brewerydb.com/v2/brewery/" + brewery[i].id + "/beers/?key=9bb3bc076d572ad09b636ac87cc944c9"
+          //MAY NOT NEED NOW! url for finding the beer information
 
 
+         $.ajax({
+          url: queryBreweryURL,
+          method: "GET",
+          headers: { "X-Requested-With": "" }
+          }).done(function(response){
+          // may need to rename response since have response up above -- i think it should be okay as response again
 
+          var beerResult = response.data;
+          var length = beerResult.length;
+          console.log(length)
+
+          for (var j = 0; j < length; j++) {
+
+          beerInfo = {"Name" : beerResult[j].style.name,
+                      "ABV" : beerResult[j].style.abvMin,
+                      "Hoppiness" : beerResult[j].style.ibuMin,
+                      "Color" : beerResult[j].style.srmMin,
+                      "Category" : beerResult[j].style.category.name,//if use
+                      "Description" : beerResult[j].style.description
+                      }//end of beerInfo object
+                      
+          beer.push(beerInfo);
+        
+          //if statements here for user input meeting criteria or call a function
+               
+          var p = $("<p>").append("Beer #" + j + " - ABV: " + beer[j].ABV + " Hoppiness: " + beer[j].Hoppiness + " Color: " + beer[j].Color + " Category " + beer[j].Category);
+          console.log(beerDiv);
+
+          beerDiv.append(p);
+          $("#beerResults").append(beerDiv);
+
+        }//end of for loop for beerResult
+        }) // end of function(response)
+      } // end of for-loop
+    };//end of beerFunction
+
+    return false;
 
   });//end of on click for results probably not right because this was used on giphy.
+
+
 
 });//end of document
