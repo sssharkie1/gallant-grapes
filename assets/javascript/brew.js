763948bdd4d88ed05447c72f067eebf71afd1e38
Julie <jccheng@live.unc.edu>
2017-01-20 22:44:38 -0500

    created a test html page for the js, moved code, changed breweryResult.name to breweryResult.brewery.name

--- assets/javascript/brew.js
+++ assets/javascript/brew.js
@@ -4,13 +4,28 @@ $(document).ready(function() {
       hoppiness = "",
       abv = 0,
       style = "",
-      zipcode = 0,
+      zipcode = 27613,
       brewery = [], // holds all brewery info
       beer = [];
 
+// THIS CODE IS AT LINE 30
+  // //whatever id is for user inputs
+  // $("#userinput").on("click", function() {
+    
+  //   //adjust ids to what individual user inputs will be
+  //   color = $("#color-input").val().trim();
+  //   hoppiness = $("#hoppiness-input").val().trim();
+  //   abv = $("#abv-input").val().trim();
+  //   style = $("#style-input").val().trim();
+  //   zipcode = $("#zipcode-input").val().trim();
 
-  //whatever id is for user inputs
-  $("#userinput").on("click", function() {
+  //   return false;
+
+  // });//end of on click for user input
+
+  //may not be document click...change class or id to whatever submit button is?
+  $(document).on("click", "#submit", function() { // changed id to #submit in the middle
+    
     //adjust ids to what individual user inputs will be
     color = $("#color-input").val().trim();
     hoppiness = $("#hoppiness-input").val().trim();
@@ -24,13 +39,7 @@ $(document).ready(function() {
     $("#abv-input").val("");
     $("#style-input").val("");
     $("#zipcode-input").val("");
-      
-    return false;
 
-  });//end of on click for user input
-
-  //may not be document click...change class or id to whatever submit button is?
-  $(document).on("click", "#example", function() {
     //clear results div or however is displayed in html
     $("#areaForResults").empty();  
         
@@ -51,14 +60,17 @@ $(document).ready(function() {
       for (var i = 0; i < breweryResult.length; i++){ // for-loop to add all breweries into an array
 
         breweryInfo = {"id" : breweryResult[i].id,
-                      "name" : breweryResult[i].name,
+                      "name" : breweryResult[i].brewery.name,
                       "streetName" : breweryResult[i].streetAddress,
                       "state" : breweryResult[i].region,
                       "locality": breweryResult[i].locality 
                       } //end of breweryInfo object
 
         brewery.push(breweryInfo); // pushes current brewery's entire info (stored as object) into array
-        console.log(breweryInfo);
+
+        $("#areaForResults").append("Brewery #" + i + "- id: " + breweryInfo.id + " name: " + breweryInfo.name + "<br>")
+
+        console.log("breweryInfo object: " + breweryInfo);
 
         console.log(brewery);
 
@@ -71,7 +83,7 @@ $(document).ready(function() {
       for (var i = 0; i < brewery.length; i++){ // for loop to make sure we go through all breweries
       
         //URL for brewery based on queryURL to get beer at brewery to then search for other input
-        var queryBreweryURL = "http://utcors1.herokuapp.com/http://api.brewerydb.com/v2/brewery/" + breweryID[i].id + "/beers/?key=9bb3bc076d572ad09b636ac87cc944c9"
+        var queryBreweryURL = "http://utcors1.herokuapp.com/http://api.brewerydb.com/v2/brewery/" + brewery[i].id + "/beers/?key=9bb3bc076d572ad09b636ac87cc944c9"
         //MAY NOT NEED NOW! url for finding the beer information
 
         $.ajax({
@@ -107,6 +119,8 @@ $(document).ready(function() {
         }) // end of function(response)
       } // end of for-loop
     };//end of beerFunction
+
+    return false;
 
   });//end of on click for results probably not right because this was used on giphy.
 
